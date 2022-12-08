a2a0e69380e68a50d4a6c7f7735297df5ace68d6
redolfosei <redolf639@gmail.com>
2022-12-08 18:54:03 +0000

    write a c program that prints something exactly, do not use printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
-#include <stdlib.h>
-/*
-* main - Function begins
+/**
+* main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+*followed by a new line, to standard error.
 *
-* Return: Always 1
+* Return: Always 1.
 */
 int main(void)
 {
-write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
