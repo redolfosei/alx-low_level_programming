b6d3e1c1c307f7fbb6ce49e389e3dcdb09627f36
redolfosei <redolf639@gmail.com>
2022-12-13 21:11:02 +0000

    Write a program that print 43

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,28 @@
-#include <stdio.h>
+#include "main.h"
 /**
- * print_sign - analyse a number if is positive or not
- * @n:is the value analysed
- * Return: is the status code
- * */
-
+* @n: An input number
+* Description: Tohis function prints the sign of the number
+* Return: 1 if number is positive, o if number is 0, or
+* -1 if number is negative
+*/
 int print_sign(int n)
 {
-int value;
-if (n > 0)
-{
-value = 1;
-printf("+");
-}
-else if (n == 0)
-{
-value = 0;
-printf("0");
-}
-else if (n < 0)
-{
-value = -1;
-printf("-");
-}
-return (value);
+	int value;
 
+	if (n > 0)
+	{
+	value = 1;
+	_putchar('+');
+	}
+	else if (n == 0)
+	{
+	value = 0;
+	_putchar('0');
+	}
+	else
+	{
+	value = -1;
+	_putchar('-');
+	}
+	return (value);
 }
