9eaf093bb32b8437c53eef0bddba94bad9b1a830
redolfosei <redolf639@gmail.com>
2022-12-13 16:52:06 +0000

    Write a program that print

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *  *_islower - tells if a data is lower or not
+ *   * @c: takes the input
+ *    * Return: returun status code
+ *     */
+
+int _islower(int c)
+{
+    int value;
+    if (islower(c))
+    {
+        value = 1;
+        
+    }
+    else
+    {
+        value = 0;
+        
+    }
+    return (value);
+    
+}
