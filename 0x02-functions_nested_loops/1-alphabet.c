3d91116d2cd76c67183f05ce60a828e0de94253e
redolfosei <redolf639@gmail.com>
2022-12-13 14:29:09 +0000

    Write a program that prints _putchar, followed by new line2222

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,17 @@
 #include "main.h"
 
+/**
+* print_alphabet - Prints a to z
+*
+* Return: Nothing
+*/
+
 void print_alphabet(void)
 {
 char mychar;
 for (mychar = 'a'; mychar <= 'z'; mychar++)
-    {
-      _putchar(mychar);
-    }
-  _putchar('\n');
+{
+_putchar(mychar);
+}
+_putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MATH_H_INCLUDED
 #define MATH_H_INCLUDED
 
-int _putchar(char c); 
+int _putchar(char c);
 void print_alphabet(void);
 
 #endif
