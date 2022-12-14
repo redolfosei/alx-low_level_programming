dcc5b3ed76326e90d97c8f68c19e74a6d96001b8
redolfosei <redolf639@gmail.com>
2022-12-14 13:44:57 +0000

    prints the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,19 +2,19 @@
 
 /**
 * print_last_digit - prints the last digit of a number
-* 
-* Return: the last digit of the number
+* @num: The numnber to accept numbers
 *
+* Return: the last digit of the number
 */
 
 int print_last_digit(int num)
 {
- int last_digit;
- last_digit = num % 10;
+int last_digit;
+last_digit = num % 10;
 
-  if(last_digit < 0)
-    last_digit *= -1;
+if (last_digit < 0)
+last_digit *= -1;
 
-   _putchar(last_digit + '0');
-  return (last_digit);
+_putchar(last_digit + '0');
+return (last_digit);
 }
