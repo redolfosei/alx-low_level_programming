b095690fd1204dc7a6317caedb160d14a944f1a1
redolfosei <redolf639@gmail.com>
2022-12-14 16:28:04 +0000

    a function that prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* add_two - Add two intergers num_1 and num_2.
+* add - Add two intergers num_1 and num_2.
 * @num_1: The first integer to consider.
 * @num_2: The second integet to conside.
 * Description: This will add two numbers and return the results.
@@ -9,7 +9,7 @@
 * Return: The results of the addition.
 */
 
-int add_two(num_1, num_2)
+int add(int num_1, int num_2)
 {
 return (num_1 + num_2);
 }
