c7b278eb7192aa8c2d561713da97f051c3aa87e9
redolfosei <redolf639@gmail.com>
2022-12-14 18:10:04 +0000

    a function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
+/**
+* File: main.h
+* Auth: Redolf Kwame Osei
+* Desc: Header file containing declaration of all
+* functions used in 0x02-functions_nested_loops
+*/
+
 int _putchar(char c);
 void print_alphabet_x10(void);
 void print_alphabet(void);
@@ -13,7 +20,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int num_1, int num_2);
 void print_to_98(int num);
-
+void print_times_table(int number);
 
 
 #endif
