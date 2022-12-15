eaa3de0aa93738eeef1b2829100d788f913c521e
redolfosei <redolf639@gmail.com>
2022-12-15 15:32:47 +0000

    a function that prints numbers from 0 - 9 do not print 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,19 @@
-#included "main.h"
+#include "main.h"
 
-void print_most_numbers(void)
+/**
+* print_numbers - a function prints the numbers, from 0 to 9,
+* followed by a new line.
+*
+* Return: Always 0.
+*/
+
+void print_numbers(void)
 {
-
+int i = '0';
+for (; i <= '9'; i++)
+{
+if (i != '2' && i != '4')
+_putchar(i);
+_putchar('\n');
+}
 }
