09b774402d981d42479afd73a4e645c1dfa1671b
redolfosei <redolf639@gmail.com>
2022-12-20 02:53:02 +0000

    a function that convert a string to an integer.

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,21 +1,21 @@
+
 #include "main.h"
+
 /**
- * _strcpy - Copies a string pointed to by @src, including the
- *           terminating null byte, to a buffer pointed to by @dest.
- * @dest: A buffer to copy the string to.
- * @src: The source string to copy.
- *
- * Return: A pointer to the destination string @dest.
- */
-char *_strcpy(char *dest, const char *src)
+* _strcpy - a function that copies the string pointed to by src,
+* including the terminating null byte (\0),
+* to the buffer pointed to by dest.
+*
+* @dest: A pointer to destination of string
+* @src: A pointer to source string to copy from
+* Return: pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	char *aux = dest;
 
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
-
-	return (dest);
+	while (*src)
+		*dest++ = *src++;
+	return (aux);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -8,4 +9,5 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char *_strcpy(char *dest, const char *src);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
