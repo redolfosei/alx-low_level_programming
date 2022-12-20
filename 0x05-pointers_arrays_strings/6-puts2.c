918e5be56baa7ea0495993a69efac86e7c7c5b17
redolfosei <redolf639@gmail.com>
2022-12-20 01:43:08 +0000

    a function that prints half of a string, followed by a new line.

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
 * puts2 - a function that prints every other character of a string,
 * starting with the first character, followed by a new line.
 * @str: An input string
 * Return: Nothing
 */
-
 void puts2(char *str)
 {
 int len = 0, i = 0;
