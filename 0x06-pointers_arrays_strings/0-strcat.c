4deb2e894dd6dda11efb4f768c22da8f51c2b7ec
redolfosei <redolf639@gmail.com>
2022-12-21 18:58:16 +0000

    a function that copies a string.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,5 @@
 
 char *_strcat(char *dest, char *src)
 {
-
 return (strcat(dest, src));
-
 }
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strncpy - copy a number of char from one string to another
+ *@dest: is the destine of the copy
+ *@src: source of the copied chars
+ *@n: number of chars that will be coied
+ * Return: return the string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+return (strncpy(dest, src, n));
+
+
+}
