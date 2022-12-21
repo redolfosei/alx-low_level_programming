61d8d1f539658920d1650b4720cb29c62126b3f1
redolfosei <redolf639@gmail.com>
2022-12-21 20:16:23 +0000

    a function that prints a buffer.

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,4 +10,5 @@ char *rot13(char *s);
 void print_number(int n);
 void rev_string(char *n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
+void print_line(char *c, int s, int l);
+void print_butter(char *b, int size);
