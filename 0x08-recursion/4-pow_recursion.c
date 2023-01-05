1ecb88761c263078b9ab7ba81f6bf775b938ad3e
redolfosei <redolf639@gmail.com>
2023-01-05 21:42:44 +0000

    a function that returns the value of x raised to the power of y.

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _pow_recursion - a function that returns the power of @x by @y x^y
+ *
+ * Description: this method is good for speed.
+ *
+ * @x: integer x
+ * @y: integer y
+ *
+ * Return: the power of @x by @y
+*/
+int _pow_recursion(int x, int y)
+{
+int even;
+if (y < 0)
+return (-1);
+else if (y == 0)
+return (1);
+else if (y % 2 == 0)
+{
+even = _pow_recursion(x, y / 2);
+return (even * even);
+}
+else
+return (x * _pow_recursion(x, y - 1));
+}
