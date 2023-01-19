3772ab9cc2f78f595777790e567a796555dc7616
redolfosei <redolf639@gmail.com>
2023-01-19 20:11:22 +0000

    2-int_index.c 2nd

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stdlib.h> //without this size_t wont work;
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
